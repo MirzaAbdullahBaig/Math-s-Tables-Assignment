94995dc51bf17b4884ac237de0c874288c5f37a4
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-04-29 12:37:12 +0500

    Not completed

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Math's Tables Assignment
+
+let input = parseInt(prompt("Please enter a digit that print the Table"));
+let table = document.getElementById(`tbody`);
+
+document.getElementById(`inp`).innerHTML = input;
+
+input.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let tableOf = input.value;
+    table.innerHTML = "";
+  
+    for (let i = 1; i <= 10; i++) {
+      table.innerHTML += `<tr">
+          <td">${tableOf}</td>
+          <td">X</td>
+          <td">${i}</td>
+          <td">= </td>
+          <td">${tableOf * i}</td>
+      </tr>`;
+    }
+    input.value = ""
+  });
