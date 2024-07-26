be807b274828cdfb5a8210c90b34d87fee5a6e45
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-07-26 20:30:08 +0500

    Convert JS to TS

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-// Math's Tables Assignment
-
-let inputform = document.getElementById(`form`);
-let input = document.getElementById(`inputNumber`);
-let table = document.getElementById(`tbody`);
-
-inputform.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let tableOf = input.value;
-  tbody.innerHTML = "";
-
-  for (let i = 1; i <= 10; i++) {
-
-    tbody.innerHTML += `<tr">
-        <td>${tableOf}</td>
-        <td>X</td>
-        <td>${i}</td>
-        <td>= </td>
-        <td>${tableOf * i}</td>
-    </tr>`;
-  }
-  document.getElementById(`inp`).innerHTML = "of " + input.value;
+var inputform = document.getElementById("form");
+var table = document.getElementById("tbody");
+inputform.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var inputform = event === null || event === void 0 ? void 0 : event.target;
+    var input = inputform === null || inputform === void 0 ? void 0 : inputform.firstElementChild;
+    var tableOf = Number(input.value);
+    table.innerHTML = "";
+    for (var i = 1; i <= 10; i++) {
+        table.innerHTML += "<tr\">\n    <td>".concat(tableOf, "</td>\n    <td>X</td>\n    <td>").concat(i, "</td>\n    <td>= </td>\n    <td>").concat(tableOf * i, "</td> \n    </tr>");
+    }
+    document.getElementById("inp").innerHTML = "of " + input.value;
 });
---
+++ app.ts
@@ -0,0 +1,24 @@
+const inputform = document.getElementById(`form`)!;
+const table = document.getElementById(`tbody`)!;
+
+inputform.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputform = event?.target as HTMLFormElement;
+  const input = inputform?.firstElementChild as HTMLInputElement;
+
+  const tableOf = Number(input.value);
+
+  table.innerHTML = "";
+
+  for (let i:number = 1; i <= 10; i++) {
+    table.innerHTML += `<tr">
+    <td>${tableOf}</td>
+    <td>X</td>
+    <td>${i}</td>
+    <td>= </td>
+    <td>${tableOf * i}</td> 
+    </tr>`;
+  }
+  document.getElementById(`inp`)!.innerHTML = "of " + input.value;
+});
